9e0c4eec480bb9ede78f706d465f9ee912be221c
soongu <naong526@naver.com>
2023-12-23 10:28:09 +0900

    #32. 회원관리 회원가입 전체 처리 완성

--- src/main/java/com/spring/mvc/chap05/controller/MemberController.java
+++ src/main/java/com/spring/mvc/chap05/controller/MemberController.java
@@ -1,13 +1,14 @@
 package com.spring.mvc.chap05.controller;
 
 
+import com.spring.mvc.chap05.dto.request.SignUpRequestDTO;
 import com.spring.mvc.chap05.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/members")
@@ -22,6 +23,25 @@ public class MemberController {
     public String signUp() {
         log.info("/members/sign-up GET : forwarding to sign-up.jsp");
         return "members/sign-up";
+    }
+
+    // 아이디, 이메일 중복체크 비동기 요청 처리
+    @GetMapping("/check")
+    @ResponseBody
+    public ResponseEntity<?> check(String type, String keyword) {
+        log.info("/members/check?type={}&keyword={} ASYNC GET", type, keyword);
+        boolean flag = memberService.checkDuplicateValue(type, keyword);
+        log.debug("중복체크 결과 : {}", flag);
+        return ResponseEntity.ok().body(flag);
+    }
+
+    // 회원가입 처리
+    @PostMapping("/sign-up")
+    public String signUp(SignUpRequestDTO dto) {
+        log.info("/member/sign-up POST !");
+        log.debug("parameter: {}", dto);
+        boolean flag = memberService.join(dto);
+        return flag ? "redirect:/board/list" : "redirect:/members/sign-up";
     }
 
 }
